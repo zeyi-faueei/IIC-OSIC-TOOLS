2d28e3a1cf23d2a8212ebe81d7b3b9207171e3e3
Harald Pretl <harald.pretl@jku.at>
2024-04-02 09:19:55 +0200

    Improve build script of yosys (do as in OpenROAD)

--- _build/images/yosys/scripts/install.sh
+++ _build/images/yosys/scripts/install.sh
@@ -7,9 +7,7 @@ REPO_COMMIT_SHORT=$(echo "$YOSYS_REPO_COMMIT" | cut -c 1-7)
 git clone --filter=blob:none "${YOSYS_REPO_URL}" "${YOSYS_NAME}"
 cd "${YOSYS_NAME}"
 git checkout "${YOSYS_REPO_COMMIT}"
-make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" config-gcc
-make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" -j"$(nproc)"
-make PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" install
+make install -j"$(nproc)" PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" CONFIG=gcc
 cd ..
 
 export PATH=$PATH:${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}/bin
@@ -18,20 +16,19 @@ export PATH=$PATH:${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}/bin
 git clone --filter=blob:none https://github.com/YosysHQ/eqy.git
 cd eqy
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
-make -j"$(nproc)"
-make install
+make install -j"$(nproc)"
 cd ..
 
 # Install yosys sby
 git clone --filter=blob:none https://github.com/YosysHQ/sby.git
 cd sby
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
-make install
+make install -j"$(nproc)" 
 cd ..
 
 # Install yosys mcy
 git clone --filter=blob:none https://github.com/YosysHQ/mcy.git
 cd mcy
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
-make install
+make install -j"$(nproc)"
 cd ..
