fffcd104b8a3637aaca69d5db496e17541dbed28
Harald Pretl <harald.pretl@jku.at>
2024-05-01 13:29:46 +0200

    Clean handling of Yosys eqy, sby and mcy

--- _build/images/yosys/scripts/install.sh
+++ _build/images/yosys/scripts/install.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# Build yosys
+# -----------
+
 REPO_COMMIT_SHORT=$(echo "$YOSYS_REPO_COMMIT" | cut -c 1-7)
 
 git clone --filter=blob:none "${YOSYS_REPO_URL}" "${YOSYS_NAME}"
@@ -12,23 +15,32 @@ cd ..
 
 export PATH=$PATH:${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}/bin
 
-# Install yosys eqy
-git clone --filter=blob:none https://github.com/YosysHQ/eqy.git
-cd eqy
+# Build yosys eqy
+# ---------------
+
+git clone --filter=blob:none ${YOSYS_EQY_REPO_URL} ${YOSYS_EQY_NAME}
+cd ${YOSYS_EQY_NAME}
+git checkout ${YOSYS_EQY_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)"
 cd ..
 
-# Install yosys sby
-git clone --filter=blob:none https://github.com/YosysHQ/sby.git
-cd sby
+# Build yosys sby
+# ---------------
+
+git clone --filter=blob:none ${YOSYS_SBY_REPO_URL} ${YOSYS_SBY_NAME}
+cd ${YOSYS_SBY_NAME}
+git checkout ${YOSYS_SBY_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)" 
 cd ..
 
 # Install yosys mcy
-git clone --filter=blob:none https://github.com/YosysHQ/mcy.git
-cd mcy
+# -----------------
+
+git clone --filter=blob:none ${YOSYS_MCY_REPO_URL} ${YOSYS_MCY_NAME}
+cd ${YOSYS_MCY_NAME}
+git checkout ${YOSYS_MCY_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)"
 cd ..
