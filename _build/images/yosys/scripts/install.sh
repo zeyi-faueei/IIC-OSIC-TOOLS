e93c64d126944142a5aebcee31b6c3941ffda407
Harald Pretl <harald.pretl@jku.at>
2024-05-22 10:50:10 +0200

    Update tool version; Yosys SBY/EQY/MCY versions now use yosys tag

--- _build/images/yosys/scripts/install.sh
+++ _build/images/yosys/scripts/install.sh
@@ -20,7 +20,7 @@ export PATH=$PATH:${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}/bin
 
 git clone --filter=blob:none ${YOSYS_EQY_REPO_URL} ${YOSYS_EQY_NAME}
 cd ${YOSYS_EQY_NAME}
-git checkout ${YOSYS_EQY_REPO_COMMIT}
+git checkout ${YOSYS_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)"
 cd ..
@@ -30,7 +30,7 @@ cd ..
 
 git clone --filter=blob:none ${YOSYS_SBY_REPO_URL} ${YOSYS_SBY_NAME}
 cd ${YOSYS_SBY_NAME}
-git checkout ${YOSYS_SBY_REPO_COMMIT}
+git checkout ${YOSYS_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)" 
 cd ..
@@ -40,7 +40,7 @@ cd ..
 
 git clone --filter=blob:none ${YOSYS_MCY_REPO_URL} ${YOSYS_MCY_NAME}
 cd ${YOSYS_MCY_NAME}
-git checkout ${YOSYS_MCY_REPO_COMMIT}
+git checkout ${YOSYS_REPO_COMMIT}
 sed -i "s#^PREFIX.*#PREFIX=${TOOLS}/${YOSYS_NAME}#g" Makefile
 make install -j"$(nproc)"
 cd ..
