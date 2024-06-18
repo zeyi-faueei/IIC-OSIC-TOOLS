3d60dc88ca46d043bfa125ce640af9cf07913a43
Harald Pretl <harald.pretl@jku.at>
2024-06-18 15:27:31 +0200

    Yosys-42 and on needs this addition

--- _build/images/yosys/scripts/install.sh
+++ _build/images/yosys/scripts/install.sh
@@ -10,6 +10,7 @@ REPO_COMMIT_SHORT=$(echo "$YOSYS_REPO_COMMIT" | cut -c 1-7)
 git clone --filter=blob:none "${YOSYS_REPO_URL}" "${YOSYS_NAME}"
 cd "${YOSYS_NAME}"
 git checkout "${YOSYS_REPO_COMMIT}"
+git submodule update --init
 make install -j"$(nproc)" PREFIX="${TOOLS}/${YOSYS_NAME}/${REPO_COMMIT_SHORT}" CONFIG=gcc
 cd ..
 
