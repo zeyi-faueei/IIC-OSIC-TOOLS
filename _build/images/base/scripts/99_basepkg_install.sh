120c4466f43113e5524f464544c27e1193dcaef5
Harald Pretl <harald.pretl@jku.at>
2024-06-25 14:04:17 +0200

    Pin major packages

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -2,28 +2,30 @@
 
 set -e
 
+#FIXME remove openlabe for now.
+# openlane==2.0.9 \
+
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
-	amaranth \
-	cace \
-	cocotb \
-	edalize \
-	fusesoc \
-	gdsfactory \
-	gdspy \
-	hdl21 \
-	openlane \
-	openram \
-	pyrtl \
-	pyspice \
-	pyuvm \
-	pyverilog \
-	schemdraw \
-	scikit-rf \
-	siliconcompiler \
-	spyci \
-	vlsirtools \
-	volare
+	amaranth==0.5.0 \
+	cace==2.3.7 \
+	cocotb==1.8.1 \
+	edalize==0.5.4 \
+	fusesoc==2.3 \
+	gdsfactory==8.3.0 \
+	gdspy==1.6.13 \
+	hdl21==6.0.0 \
+	openram==1.2.48 \
+	pyrtl==0.11.1 \
+	pyspice==1.5 \
+	pyuvm==2.91 \
+	pyverilog==1.3.0 \
+	schemdraw==0.19 \
+	scikit-rf==1.1.0 \
+	siliconcompiler==0.23.2 \
+	spyci==1.0.2 \
+	vlsirtools==6.0.0 \
+	volare==0.18.0
 
 pip3 install --upgrade --no-cache-dir \
 	jupyter \
