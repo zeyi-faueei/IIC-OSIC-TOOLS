ae5178dcc0a6ce38cf12c2c39183c98298045334
Harald Pretl <harald.pretl@jku.at>
2024-07-05 12:24:28 +0200

    Install OpenLane2, brick gdsfactory dependency by doing that (hopefully this will be resolved in the future)

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -9,14 +9,13 @@ pip3 install --upgrade --no-cache-dir \
 	jupyter-collaboration \
 	jupyterlab-night
 
-#FIXME openlane==2.0.10 \
 #FIXME Locally build OpenLane2 due to klayout incompatibility
 #FIXME Need to add OL2 first, otherwise dependency issue on antlr4-python3-runtime
-cd /tmp
-git clone --depth=1 https://github.com/iic-jku/openlane2.git
-cd openlane2
-pip3 install .
-cd /
+#cd /tmp
+#git clone --depth=1 https://github.com/iic-jku/openlane2.git
+#cd openlane2
+#pip3 install .
+#cd /
 
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
@@ -39,6 +38,10 @@ pip3 install --upgrade --no-cache-dir \
 	spyci==1.0.2 \
 	vlsirtools==6.0.0 \
 	volare==0.18.1
+
+#FIXME OpenLane2 goes last to get its dependencies right
+pip3 install --upgrade --no-chache-dir \
+	openlane==2.0.11
 
 echo "[INFO] Install EDA packages via GEM"
 gem install \
