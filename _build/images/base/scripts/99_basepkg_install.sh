1966ef11db2c8de0aa3780de3e395bd6407e8761
Harald Pretl <harald.pretl@jku.at>
2024-07-05 10:32:11 +0200

    Bump Python packages

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -21,24 +21,24 @@ cd /
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
 	amaranth==0.5.0 \
-	cace==2.3.7 \
+	cace==2.3.9 \
 	cocotb==1.8.1 \
 	edalize==0.5.4 \
 	fusesoc==2.3 \
-	gdsfactory==8.3.1 \
+	gdsfactory==8.4.0 \
 	gdspy==1.6.13 \
 	hdl21==6.0.0 \
 	openram==1.2.48 \
 	pyrtl==0.11.1 \
 	pyspice==1.5 \
-	pyuvm==2.9.1 \
+	pyuvm==3.0.0 \
 	pyverilog==1.3.0 \
 	schemdraw==0.19 \
 	scikit-rf==1.1.0 \
-	siliconcompiler==0.23.3 \
+	siliconcompiler==0.23.4 \
 	spyci==1.0.2 \
 	vlsirtools==6.0.0 \
-	volare==0.18.0
+	volare==0.18.1
 
 echo "[INFO] Install EDA packages via GEM"
 gem install \
