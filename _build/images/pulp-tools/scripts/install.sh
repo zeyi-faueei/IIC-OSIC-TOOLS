cfdee86ca5d53abe09860ee0e02c50659b5d5ee9
Harald Pretl <harald.pretl@jku.at>
2024-07-20 21:33:06 +0200

    Fix typo and shellcheck warnings

--- _build/images/pulp-tools/scripts/install.sh
+++ _build/images/pulp-tools/scripts/install.sh
@@ -8,22 +8,22 @@ mkdir -p "${TOOLS}/${PULP_NAME}/bin"
 # ------------
 git clone --depth=1 https://github.com/pulp-platform/bender.git
 cd bender || exit
-cargo build --release -j$(nproc)
-copy target/release/bender "${TOOLS}/${PULP_NAME}/bin"
+cargo build --release -j"$(nproc)"
+cp target/release/bender "${TOOLS}/${PULP_NAME}/bin"
 cd ..
 
 # Build Morty
 # -----------
 git clone --depth=1 https://github.com/pulp-platform/morty.git 
 cd morty || exit
-cargo build --release -j$(nproc)
-copy target/release/morty "${TOOLS}/${PULP_NAME}/bin"
+cargo build --release -j"$(nproc)"
+cp target/release/morty "${TOOLS}/${PULP_NAME}/bin"
 cd ..
 
 # Build SVase
 # -----------
 git clone --depth=1 https://github.com/pulp-platform/svase.git
 cd svase || exit
-make build -j$(nproc)
-copy build/svase "${TOOLS}/${PULP_NAME}/bin"
+make build -j"$(nproc)"
+cp build/svase "${TOOLS}/${PULP_NAME}/bin"
 cd ..
