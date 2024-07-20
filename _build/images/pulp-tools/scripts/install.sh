b20744064317be8889ff0ea90b91ae597bc106a5
Harald Pretl <harald.pretl@jku.at>
2024-07-20 15:34:19 +0200

    Adding PULP tools bender, morty, and svase

---
+++ _build/images/pulp-tools/scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p "${TOOLS}/${PULP_NAME}/bin"
+
+# Build Bender
+# ------------
+git clone --depth=1 https://github.com/pulp-platform/bender.git
+cd bender || exit
+cargo build --release -j$(nproc)
+copy target/release/bender "${TOOLS}/${PULP_NAME}/bin"
+cd ..
+
+# Build Morty
+# -----------
+git clone --depth=1 https://github.com/pulp-platform/morty.git 
+cd morty || exit
+cargo build --release -j$(nproc)
+copy target/release/morty "${TOOLS}/${PULP_NAME}/bin"
+cd ..
+
+# Build SVase
+# -----------
+git clone --depth=1 https://github.com/pulp-platform/svase.git
+cd svase || exit
+make build -j$(nproc)
+copy build/svase "${TOOLS}/${PULP_NAME}/bin"
+cd ..
