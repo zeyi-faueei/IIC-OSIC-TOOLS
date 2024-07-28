73a69b8c4d92c392424e9485f9d1a69c92ffe4f4
Harald Pretl <harald@pretl.eu>
2024-07-28 21:58:59 +0200

    Adapt build script per https://github.com/RTimothyEdwards/XCircuit/issues/21#issuecomment-2254620012

--- _build/images/xcircuit/scripts/install.sh
+++ _build/images/xcircuit/scripts/install.sh
@@ -5,6 +5,7 @@ cd /tmp || exit 1
 git clone --filter=blob:none "${XCIRCUIT_REPO_URL}" "${XCIRCUIT_NAME}"
 cd "${XCIRCUIT_NAME}" || exit 1
 git checkout "${XCIRCUIT_REPO_COMMIT}"
+aclocal && automake && autoconf
 ./configure --prefix="${TOOLS}/${XCIRCUIT_NAME}"
 make -j"$(nproc)"
 make install
