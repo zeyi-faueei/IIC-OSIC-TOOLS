4e9c1c83b8b5437298e5c6e51132ced8e61f44b8
Harald Pretl <harald.pretl@jku.at>
2024-09-07 15:54:41 +0200

    Bump tool version

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -11,14 +11,14 @@ pip3 install --upgrade --no-cache-dir \
 
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
-	amaranth==0.5.1 \
-	cace==2.4.5 \
-	cocotb==1.9.0 \
+	amaranth==0.5.2 \
+	cace==2.4.14 \
+	cocotb==1.9.1 \
 	edalize==0.5.4 \
 	fusesoc==2.3 \
-	gdsfactory==8.7.2 \
+	gdsfactory==8.8.5 \
 	gdspy==1.6.13 \
-	openlane==2.1.2 \
+	openlane==2.1.3 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
 	pyrtl==0.11.2 \
@@ -26,10 +26,10 @@ pip3 install --upgrade --no-cache-dir \
 	pyuvm==3.0.0 \
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.19 \
-	scikit-rf==1.2.0 \
-	siliconcompiler==0.26.1 \
+	scikit-rf==1.3.0 \
+	siliconcompiler==0.27.1 \
 	spyci==1.0.2 \
-	volare==0.18.1
+	volare==0.19.1
 
 echo "[INFO] Install EDA packages via GEM"
 gem install \
