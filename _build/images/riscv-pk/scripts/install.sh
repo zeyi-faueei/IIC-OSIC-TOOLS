a4bbdd01815ac031e1c7545fa9a0b317bc8c390e
Harald Pretl <harald@pretl.eu>
2024-09-15 10:42:20 +0200

    Add path to fix build fail

--- _build/images/riscv-pk/scripts/install.sh
+++ _build/images/riscv-pk/scripts/install.sh
@@ -2,6 +2,8 @@
 set -e
 cd /tmp || exit 1
 
+export PATH="$RISCV/bin:$PATH"
+
 git clone --filter=blob:none "${RISCV_PK_REPO_URL}" "${RISCV_PK_NAME}"
 cd "${RISCV_PK_NAME}" || exit 1
 git checkout "${RISCV_PK_REPO_COMMIT}"
