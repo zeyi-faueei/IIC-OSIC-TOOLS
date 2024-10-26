5118cc632c694e24b8c2c023b9a9d79f7d102b07
Harald Pretl <harald@pretl.eu>
2024-10-26 19:55:45 +0200

    Bump Python pkgs version

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -12,14 +12,14 @@ pip3 install --upgrade --no-cache-dir \
 #FIXME gdfsfactory >= 8.8.6 requires Python 3.11
 echo "[INFO] Install EDA packages via PIP"
 pip3 install --upgrade --no-cache-dir \
-	amaranth==0.5.2 \
-	cace==2.4.14 \
+	amaranth==0.5.3 \
+	cace==2.5.3 \
 	cocotb==1.9.1 \
 	edalize==0.5.4 \
-	fusesoc==2.3 \
-	gdsfactory==8.8.2 \
+	fusesoc==2.4 \
+	gdsfactory==8.15.0 \
 	gdspy==1.6.13 \
-	openlane==2.1.4 \
+	openlane==2.2.1 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
 	pyrtl==0.11.2 \
@@ -28,9 +28,9 @@ pip3 install --upgrade --no-cache-dir \
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.19 \
 	scikit-rf==1.3.0 \
-	siliconcompiler==0.27.1 \
+	siliconcompiler==0.28.3 \
 	spyci==1.0.2 \
-	volare==0.19.1
+	volare==0.19.2
 
 echo "[INFO] Install EDA packages via GEM"
 gem install \
