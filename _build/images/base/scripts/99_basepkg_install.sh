ce30f89db1ec37cff85ad86954cc0f1a8f6c30f2
Harald Pretl <harald@pretl.eu>
2024-11-16 08:44:58 +0100

    Bump tool version

--- _build/images/base/scripts/99_basepkg_install.sh
+++ _build/images/base/scripts/99_basepkg_install.sh
@@ -15,11 +15,11 @@ pip3 install --upgrade --no-cache-dir \
 	amaranth==0.5.3 \
 	cace==2.5.3 \
 	cocotb==1.9.2 \
-	edalize==0.5.4 \
+	edalize==0.6.0 \
 	fusesoc==2.4 \
-	gdsfactory==8.17.0 \
+	gdsfactory==8.19.1 \
 	gdspy==1.6.13 \
-	openlane==2.2.3 \
+	openlane==2.2.5 \
 	openram==1.2.48 \
 	pygmid==1.2.12 \
 	pyrtl==0.11.2 \
@@ -27,8 +27,8 @@ pip3 install --upgrade --no-cache-dir \
 	pyuvm==3.0.0 \
 	pyverilog==1.3.0 \
 	schemdraw[svgmath]==0.19 \
-	scikit-rf==1.3.0 \
-	siliconcompiler==0.28.4 \
+	scikit-rf==1.4.1 \
+	siliconcompiler==0.28.7 \
 	spyci==1.0.2 \
 	volare==0.19.2
 
