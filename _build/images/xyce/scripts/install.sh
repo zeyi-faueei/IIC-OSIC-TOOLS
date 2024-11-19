5e2e50fa3c531ecb28fa2a79e6d1b491096c72e1
Harald Pretl <harald.pretl@jku.at>
2024-11-19 18:04:25 +0100

    Improve trilinos repo and version handling

--- _build/images/xyce/scripts/install.sh
+++ _build/images/xyce/scripts/install.sh
@@ -7,9 +7,9 @@ cd "${XYCE_NAME}" || exit 1
 git checkout "${XYCE_REPO_COMMIT}"
 ./bootstrap
 
-git clone --filter=blob:none https://github.com/trilinos/Trilinos trilinos
-cd trilinos || exit 1
-git checkout trilinos-release-12-12-1
+git clone --filter=blob:none "${XYCE_TRILINOS_REPO_URL}" "${XYCE_TRILINOS_NAME}"
+cd "${XYCE_TRILINOS_NAME}" || exit 1
+git checkout "${XYCE_TRILINOS_REPO_COMMIT}"
 mkdir -p parallel_build && cd parallel_build
 cp /images/xyce/scripts/trilinos.reconfigure.sh ./reconfigure.sh
 chmod +x reconfigure.sh
