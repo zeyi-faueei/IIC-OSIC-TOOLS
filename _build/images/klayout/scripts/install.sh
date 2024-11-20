c6d7015d427077ec94549c5b58634508d22f22f3
Harald Pretl <harald@pretl.eu>
2024-11-20 09:16:42 +0100

    KLayout patch to fix thick black selection line

--- _build/images/klayout/scripts/install.sh
+++ _build/images/klayout/scripts/install.sh
@@ -5,6 +5,9 @@ cd /tmp || exit 1
 git clone --filter=blob:none "${KLAYOUT_REPO_URL}" "${KLAYOUT_NAME}"
 cd "${KLAYOUT_NAME}" || exit 1
 git checkout "${KLAYOUT_REPO_COMMIT}"
+#FIXME Apply patch to fix https://github.com/KLayout/klayout/issues/1930 until it is rolled into release
+sed -i 's/char m_line/int m_line/' src/laybasic/laybasic/layMarker.h
+#FIXME
 prefix=${TOOLS}/${KLAYOUT_NAME}
 mkdir -p "$prefix"
 ./build.sh -j"$(nproc)" -prefix "$prefix" -without-qtbinding
